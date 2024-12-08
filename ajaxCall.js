9cc745b0901451bd3faecdff79e06e3cb12e5ccb
AlonTsoran <alontsoranschool@gmail.com>
2024-12-08 15:04:28 +0200

    ששש

--- ajaxCall.js
+++ ajaxCall.js
@@ -9,3 +9,15 @@ function ajaxCall(method, api, data, successCB, errorCB) {
   });
 }
 let api = "https://194.90.158.74/igroup6/test2/tar1/api/Game";
+
+function logout() {
+  localStorage.removeItem("userEmail");
+  localStorage.removeItem("userName");
+  window.location.href = "./login.html";
+}
+function checkIfLoggedIn() {
+  const userEmail = localStorage.getItem("userEmail");
+  if (!userEmail) {
+    window.location.href = "./login.html";
+  }
+}
